3ab6838e0779910e9d76e42ebe668ebaad6bf9c3
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2023-10-25 13:05:39 +0900

    디펜던시 설치
    - react-native navigation, react-native-vector-icons 설치

---
+++ src/components/Start.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const Start = () => {
+  return (
+    <View>
+      <Icon name="arrow-back" size={30} color="#900" />
+      <Text>Start</Text>
+    </View>
+  );
+};
+
+export default Start;
