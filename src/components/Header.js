73a8fb97c00093d94c80a87b0ee7dc4658da48c2
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2023-10-26 11:24:34 +0900

    Feed 페이지 추가
    - Feed 페이지 추가
    - Header.js Android 카메라 홀 겹침 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
-import {Pressable, StyleSheet, View} from 'react-native';
+import {Image, Platform, Pressable, StyleSheet, View} from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
+import {useScrollToTop} from '@react-navigation/native';
 
 const Header = ({navigation}) => {
+  const ref = useRef(null);
+
+  useScrollToTop(
+    useRef({scrollToTop: () => ref.current?.scrollToTop({y: 100})}),
+  );
+
   return (
     <View style={styles.header}>
       <Pressable onPress={() => navigation.openDrawer()}>
-        <Icon name="user" size={30} color="#1DA1f2" style={styles.userIcon} />
+        <Image style={styles.userIcon} source={{uri: PROFILE_URI}} />
       </Pressable>
       <Icon name="twitter" size={30} color="#1DA1f2" />
       <Pressable onPress={() => navigation.navigate('Feed Setting')}>
-        <Icon name="setting" size={30} color="black" />
+        <Icon name="setting" size={30} color="#777777" />
       </Pressable>
     </View>
   );
@@ -23,15 +30,25 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    height: 40,
-    paddingLeft: 8,
-    paddingRight: 8,
+    alignItems: 'center',
+    height: 50,
+    paddingHorizontal: 8,
     borderBottomWidth: 1,
     borderBottomColor: '#c8c8c8',
+    ...Platform.select({
+      android: {
+        marginTop: 16,
+      },
+    }),
   },
   userIcon: {
+    width: 40,
+    height: 40,
     borderWidth: 1,
-    borderRadius: 15,
-    borderColor: '#1DA1f2',
+    borderRadius: 20,
+    borderColor: '#c8c8c8',
   },
 });
+
+const PROFILE_URI =
+  'https://i.namu.wiki/i/qCVlq5dYOT_0IRa0NwnYktixrJZMMlEzpe5RVTCEU6GSbzNSRZCZpOMoYp5QMTFy9P22ptEMyfG3Fog3mF8FBvtyum6JxExeReg2_X1J3KRcQ7b1EO67zrstqJ4dP1BIh1IvHKlrrak3I_3YeRFMlA.webp';
