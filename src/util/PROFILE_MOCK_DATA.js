03fa4547f1742b5268144e9f461d9c69ead560e2
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2023-10-30 11:54:23 +0900

    Profile.js 추가

---
+++ src/util/PROFILE_MOCK_DATA.js
@@ -0,0 +1,11 @@
+const PROFILE_MOCK_DATA = {
+  profileImgUrl:
+    'https://i.namu.wiki/i/qCVlq5dYOT_0IRa0NwnYktixrJZMMlEzpe5RVTCEU6GSbzNSRZCZpOMoYp5QMTFy9P22ptEMyfG3Fog3mF8FBvtyum6JxExeReg2_X1J3KRcQ7b1EO67zrstqJ4dP1BIh1IvHKlrrak3I_3YeRFMlA.webp',
+  backgroundImgUrl:
+    'https://photo.coolenjoy.co.kr/bbs/data/multi_photo/26/604944011_84f8d2e6_b1024x76829premium512.gif',
+  user: '몽자',
+  userId: 'mongja',
+  joinedAt: '2022-10-30T10:09:30+09:00',
+};
+
+export default PROFILE_MOCK_DATA;
--- src/view/Profile.js
+++ src/view/Profile.js
@@ -1,9 +1,86 @@
 import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
 
-import {Text, View} from 'react-native';
+import PROFILE_MOCK_DATA from '../util/PROFILE_MOCK_DATA';
+import dayjs from 'dayjs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Profile = () => {
-  return <Text>Profile View</Text>;
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image
+          style={styles.backgroundImg}
+          source={{
+            uri: PROFILE_MOCK_DATA.backgroundImgUrl,
+          }}
+        />
+        <View style={styles.profileImgView}>
+          <Image
+            style={styles.profileImg}
+            source={{uri: PROFILE_MOCK_DATA.profileImgUrl}}
+          />
+        </View>
+      </View>
+      <View style={styles.profileTextContainer}>
+        <Text style={styles.user}>{PROFILE_MOCK_DATA.user}</Text>
+        <Text style={styles.userId}>@{PROFILE_MOCK_DATA.userId}</Text>
+        <View style={styles.joinedAtContainer}>
+          <Icon name="calendar-outline" size={14} style={styles.calendarIcon} />
+          <Text style={styles.joinedAt}>
+            {dayjs(PROFILE_MOCK_DATA.joinedAt).format('YYYY년 MM월에 가입함')}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
 };
 
 export default Profile;
+
+const styles = StyleSheet.create({
+  container: {height: '100%', backgroundColor: 'white'},
+  backgroundImg: {
+    position: 'relative',
+    width: '100%',
+    height: 100,
+  },
+  profileImgView: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    bottom: -29,
+    left: 16,
+    width: 58,
+    height: 58,
+    borderRadius: 29,
+    backgroundColor: 'white',
+  },
+  profileImg: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: '#c8c8c8',
+  },
+  profileTextContainer: {
+    marginTop: 44,
+    paddingHorizontal: 16,
+  },
+  user: {
+    fontSize: 15,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  userId: {marginTop: 4, fontSize: 15, color: '#555555'},
+  joinedAtContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  calendarIcon: {marginBottom: 4, color: '#666666'},
+  joinedAt: {marginLeft: 4, fontSize: 13, color: '#666666', lineHeight: 13},
+  descContainer: {
+    paddingVertical: 8,
+  },
+});
