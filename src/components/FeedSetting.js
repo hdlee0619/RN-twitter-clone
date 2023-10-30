5cf09568ad7dfb6a20c0f68e55c018c94ac9a0f8
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2023-10-30 14:00:37 +0900

    FeedSetting.js switch component 추가

--- src/components/FeedSetting.js
+++ src/components/FeedSetting.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Text} from 'react-native';
+import {StyleSheet, Switch, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const FeedSetting = () => {
-  return <Text>FeedSettings</Text>;
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const toggleSwitch = () => {
+    setIsEnabled(prev => !prev);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.settingContainer}>
+        <Icon name="switcher" size={16} />
+        <Text style={styles.settingText}>Switch component test</Text>
+        <Switch onValueChange={toggleSwitch} value={isEnabled} />
+      </View>
+    </View>
+  );
 };
 
 export default FeedSetting;
+
+const styles = StyleSheet.create({
+  container: {backgroundColor: 'white', height: '100%'},
+  settingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#c8c8c8',
+  },
+  settingText: {
+    flex: 1,
+    marginLeft: 8,
+    fontSize: 16,
+  },
+});
